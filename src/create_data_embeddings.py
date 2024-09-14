04b8be6791a643673073e59e0160149c38b1ae8e
Sarthak Arora <58847609+iasarthak@users.noreply.github.com>
2024-09-14 12:49:28 +0530

    code improvements

--- src/create_data_embeddings.py
+++ src/create_data_embeddings.py
@@ -1,23 +1,28 @@
 import os
+import uuid
 
 import pandas as pd
-from fastembed import TextEmbedding
+from fastembed import TextEmbedding, ImageEmbedding
 from qdrant_client import QdrantClient, models
 
-from src.utils import convert_text_to_embeddings, convert_image_to_embeddings
+from src.utils import convert_text_to_embeddings, convert_image_to_embeddings, TEXT_MODEL_NAME, IMAGE_MODEL_NAME
 
-data_path = '/Users/sarthak/Documents/Work/Personal_Projects/healthcare_multimodal_ai/data/'
+DATA_PATH = '/Users/sarthak/Documents/Work/Personal_Projects/healthcare_multimodal_ai/data/'
 COLLECTION_NAME = "medical_images_text"
-TEXT_MODEL_NAME = "Qdrant/clip-ViT-B-32-text"
+
+
+def create_uuid_from_image_id(image_id):
+    NAMESPACE_UUID = uuid.UUID('12345678-1234-5678-1234-567812345678')
+    return str(uuid.uuid5(NAMESPACE_UUID, image_id))
 
 
 def create_embeddings():
     # Read captions txt data
-    path = data_path + 'captions.txt'
+    path = DATA_PATH + 'captions.txt'
     caption_df = pd.read_csv(path, sep='\t', header=None, names=['image_id', 'caption'])
 
     # Read images
-    image_directory = os.listdir(data_path + 'images')
+    image_directory = os.listdir(DATA_PATH + 'images')
 
     # Filter out images that are not in the captions
     images = []
@@ -30,33 +35,35 @@ def create_embeddings():
     for image in images:
         image_id = image.split('.')[0]
         caption = caption_df[caption_df['image_id'] == image_id]['caption'].values[0]
-        image_path = data_path + 'images/' + image
+        image_path = DATA_PATH + 'images/' + image
         image_docs.append({'image_id': image_id, 'caption': caption, 'image_path': image_path})
 
     # Convert text to embeddings using Fastembed and images to embeddings using CLIP
-    embeddings = convert_text_to_embeddings([doc['caption'] for doc in image_docs])
+    captions = [doc['caption'] for doc in image_docs]
+    embeddings = convert_text_to_embeddings(captions)
     for idx, embedding in enumerate(embeddings):
         image_docs[idx]['caption_embedding'] = embedding
 
     # Convert image to embeddings using CLIP
-    image_embeddings = []
-    for doc in image_docs:
-        image_embeddings.append(convert_image_to_embeddings(doc['image_path']))
+    image_embeddings = convert_image_to_embeddings([doc['image_path'] for doc in image_docs])
 
     for idx, embedding in enumerate(image_embeddings):
         image_docs[idx]['image_embedding'] = embedding
 
     # Save the embeddings to vector database
-    client = QdrantClient("http://localhost:6333")
+    client = QdrantClient(":memory:")
 
     text_model = TextEmbedding(model_name=TEXT_MODEL_NAME)
     text_embeddings_size = text_model._get_model_description(TEXT_MODEL_NAME)["dim"]
+
+    image_model = ImageEmbedding(model_name=IMAGE_MODEL_NAME)
+    image_embeddings_size = image_model._get_model_description(IMAGE_MODEL_NAME)["dim"]
 
     if not client.collection_exists(COLLECTION_NAME):
         client.create_collection(
             collection_name=COLLECTION_NAME,
             vectors_config={
-                "image": models.VectorParams(size=512, distance=models.Distance.COSINE),
+                "image": models.VectorParams(size=image_embeddings_size, distance=models.Distance.COSINE),
                 "text": models.VectorParams(size=text_embeddings_size, distance=models.Distance.COSINE),
             }
         )
@@ -64,16 +71,32 @@ def create_embeddings():
         collection_name=COLLECTION_NAME,
         points=[
             models.PointStruct(
-                id=doc['image_id'],
+                # Convert image_id to UUID
+                id=create_uuid_from_image_id(doc['image_id']),
                 vector={
                     "text": doc['caption_embedding'],
                     "image": doc['image_embedding'],
                 },
-                payload=doc  # original image and its caption
+                payload={
+                    "image_id": doc['image_id'],
+                    "caption": doc['caption'],
+                    "image_path": doc['image_path']
+                }
             )
             for doc in image_docs
         ]
     )
+
+    search_query = text_model.embed(["I'm facing some issue in my spine, what issue I might be facing?"])
+    # Search for the image and export it to the output folder
+    search_results = client.search(
+        collection_name=COLLECTION_NAME,
+        query_vector=('text', list(search_query)[0]),
+        with_payload=['image_path'],
+        limit=3,
+    )
+
+    print(search_results)
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,56 +1,22 @@
-# Convert text to embeddings using Fastembed
-import clip
-import torch
-from PIL import Image
-from fastembed import TextEmbedding
+from typing import List
+from fastembed import TextEmbedding, ImageEmbedding
 
 TEXT_MODEL_NAME = "Qdrant/clip-ViT-B-32-text"
+IMAGE_MODEL_NAME = "Qdrant/clip-ViT-B-32-vision"
 
 
-def convert_text_to_embeddings(text_document):
-    embedding_model = TEXT_MODEL_NAME
+def convert_text_to_embeddings(documents: List[str], embedding_model: str = TEXT_MODEL_NAME) -> List:
     text_embedding_model = TextEmbedding(model_name=embedding_model)
-    text_embeddings = text_embedding_model.embed(text_document)  # Returns a generator of embeddings
+    text_embeddings = list(text_embedding_model.embed(documents))  # Returns a generator of embeddings
     return text_embeddings
 
 
-def convert_image_to_embeddings(image_path: str):
-    # Load the model
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model, preprocess = clip.load("ViT-B/32", device=device)
-
-    # Load the image
-    image = Image.open(image_path)
-    image = preprocess(image).unsqueeze(0).to(device)
-
-    return image
+def convert_image_to_embeddings(images: List[str], embedding_model: str = IMAGE_MODEL_NAME) -> List:
+    image_model = ImageEmbedding(model_name=embedding_model)
+    images_embedded = list(image_model.embed(images))
+    return images_embedded
 
 
 if __name__ == "__main__":
-    # text_docs = ["Hello, world!", "This is a test sentence."]
-    # embeddings = convert_text_to_embeddings(text_docs)
-    # for embedding in embeddings:
-    #     print(embedding)
-
-    image_path = "/Users/sarthak/Documents/Work/Personal_Projects/healthcare_multimodal_ai/data/images/ROCO_00796.jpg"
+    image_path = ["/Users/sarthak/Documents/Work/Personal_Projects/healthcare_multimodal_ai/data/images/ROCO_81399.jpg"]
     image_embedding = convert_image_to_embeddings(image_path)
-
-    descriptions = [
-        "A Pictrue of a Tiger and Girl on Rocks",
-        "A picture of Donkey and a Man",
-        "A picture of a red car",
-        "A picture of a Sparrow and Butterfly",
-        "A picture of Animal and Human",
-    ]
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model, preprocess = clip.load("ViT-B/32", device=device)
-
-    text = clip.tokenize(descriptions).to(device)
-    with torch.no_grad():
-        logits_per_image, logits_per_text = model(image_embedding, text)
-        probs = logits_per_image.softmax(dim=-1).cpu().numpy()
-
-    results = dict(zip(descriptions, map(lambda x: x * 100, probs[0])))
-    results = {k: v for k, v in sorted(results.items(), key=lambda item: item[1], reverse=True)}  # Sorted Results
-    for text, percentage in results.items():
-        print(f"Description: {text}, Similarity: {percentage}")
